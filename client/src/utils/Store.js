7575dc05776081416963b1d516d2f51b809ff812
Jesús Hernández <Jahl-v2@hotmail.com>
2019-03-23 17:48:30 -0600

    fixes

--- client/src/utils/Store.js
+++ client/src/utils/Store.js
@@ -1,15 +1,23 @@
-/* 
+/*
 Store
 
 Utility class that saves and retreives information from the local storage
 */
 class Store {
   static set(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
+    if(value) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+    console.warn(value, " is not a valid value for the store.");
   }
 
   static get(key) {
-    return JSON.parse(localStorage.getItem(key));
+    const value = localStorage.getItem(key);
+    if(value) {
+      return JSON.parse(value);
+    }
+    console.warn(`No valid value for key ${key}, empty object returned.`);
+    return {};
   }
 }
 
